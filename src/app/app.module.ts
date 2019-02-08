f4b68c333e8cb07751a1af6512cd6453d10b70ae
Rene Manzano <rob_rene199027@hotmail.com>
2019-02-07 22:51:38 -0600

    Registro y login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+//import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// componentes
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/typings.d.ts
@@ -0,0 +1,9 @@
+
+declare var jQuery:any;
+declare var $:any;
+declare var module: NodeModule;
+
+
+interface NodeModule {
+  id: string;
+}
